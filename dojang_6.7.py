489cb3c25184d40719c06a50cc02f21a14b22217
kimnamhyeon <knh306@naver.com>
2022-09-15 18:40:29 +0900

    Feature_Chapter06

---
+++ dojang_6.7.py
@@ -0,0 +1,4 @@
+a = 50
+b = 100
+c = None
+print(a, b, c)
---
+++ dojang_6.8.py
@@ -0,0 +1,3 @@
+language, english, math, science = map(int, input().split())
+avg = int((language + english + math + science) / 4)
+print(avg)
